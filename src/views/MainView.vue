4efaa00926ae30c3605e9a54a3c0f682217082bd
mariomig837 <mariomig837@gmail.com>
2024-12-25 20:27:38 -0400

    Crea componente lista de trareas y otros varios

---
+++ src/views/MainView.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container">
+    <Sidebar></Sidebar>
+    <div class="content">
+        <router-view></router-view>
+
+    </div>
+</div>
+</template>
+
+
+<script setup>
+import Sidebar from '../components/Sidebar.vue';
+</script>
+
+
+<style>
+.container {
+    display: flex;
+}
+
+.contenido {
+    flex: 1;
+    padding: 20px;
+}
+</style>
